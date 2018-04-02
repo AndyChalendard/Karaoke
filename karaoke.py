301d11785b7b581bd11ba3aee630eefc56b600b3
Unknown <andy.chal@orange.fr>
2018-04-02 18:05:50 +0200

    Définition de la zone de texte
    Mise en place des différents message

--- karaoke.py
+++ karaoke.py
@@ -15,6 +15,10 @@ class MainWindows(QDialog, mainWindows.Ui_Dialog):
         self.setupUi(self)
         self.progressBar.setValue(0)
 
+    def textBrowserSetText(self):
+        global audioFile
+        self.textBrowser.setText("Fichier selectionne: "+ str(audioFile));
+
     #Rechercher un nouveau fichier audio
     def fileSearchClick(self):
         file_dialog = QFileDialog(self)
@@ -26,29 +30,30 @@ class MainWindows(QDialog, mainWindows.Ui_Dialog):
         #ouverture de la boite de dialogue
         if (file_dialog.exec_()):
             #Définition de la variable global contenant le fichier à lire
-            global AudioFile
-            AudioFile = str(file_dialog.selectedFiles().first())
+            global audioFile
+            audioFile = str(file_dialog.selectedFiles().first())
+            self.textBrowserSetText();
 
     # Ecouter la musique à lire
     def ecouterClick(self):
         # si aucun fichier n'à été définit
-        if (AudioFile == None):
+        if (audioFile == None):
             # on ouvre la boite de dialogue demandant le fichier
             self.fileSearchClick()
 
         # on écoute ensuite le fichier
-        if (AudioFile != None):
-            Audio.lecturefichier(AudioFile)
+        if (audioFile != None):
+            Audio.lecturefichier(audioFile)
 
     # On veut jouer sur la musique actuelle
     def jouerClick(self):
         # si aucun fichier n'à été définit
-        if (AudioFile == None):
+        if (audioFile == None):
             # on ouvre la boite de dialogue demandant le fichier
             self.fileSearchClick()
 
         # on joue sur le fichier
-        if (AudioFile != None):
+        if (audioFile != None):
             print("jouer")
 
 # si ce fichier correpond au fichier d'exécution python
@@ -59,7 +64,8 @@ if __name__ == "__main__":
     form.show()
 
     # on définit le fichier audio
-    AudioFile = None;
+    audioFile = None;
+    form.textBrowser.setText("Bienvenue sur Karaoke !\n Aucun fichier audio a ete selectionne !");
 
     # on exécute l'application
     app.exec_()
