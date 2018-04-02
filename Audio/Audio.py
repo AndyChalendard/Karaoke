d91431a7f0c757f30cb383738f5ffa7b9ae30030
DKTPasquall <pascal.ruffie3@gmail.com>
2018-04-02 18:24:46 +0200

    partie audio callback
    lecture fichier audio

--- Audio/Audio.py
+++ Audio/Audio.py
@@ -1,33 +1,41 @@
 import pyaudio
 import wave
+import time
 
 
 #phase 1: lecture d un fichier
 def lecturefichier(fichieralire) :
-    CHUNK = 1024
+   
     lecfichier=wave.open(fichieralire,'r')      #on l ouvre en lecture seule
     p=pyaudio.PyAudio()         #necessaire pr le fonctionnement
     
     
-    stream = p.open(format=p.get_format_from_width(lecfichier.getsampwidth()),
-                channels=lecfichier.getnchannels(),
-                rate=lecfichier.getframerate(),
-                output=True)            #cree le flux desire
+    def callback(in_data, frame_count, time_info, status):
+        data = wf.readframes(frame_count)
+        return (data, pyaudio.paContinue)
+    
+    stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
+                channels=wf.getnchannels(),
+                rate=wf.getframerate(),
+                output=True,
+                stream_callback=callback)            #cree le flux desire
                 
 
-    data = lecfichier.readframes(CHUNK)    #lecture/recuperation des donnees du fichier
-
-    while len(data) > 0:            #lecture du flux
-        print("on nest pas la pour etre ici")
-        stream.write(data)
-        data = lecfichier.readframes(CHUNK)
+    stream.start_stream()       
+    
+    while stream.is_active():
+        time.sleep(0.1)
 
 
-    stream.stop_stream()     #fermeture du flux
+    stream.stop_stream()                #arret du stream
     stream.close()
+    lecfichier.close()
 
     p.terminate()           #fermeture de l outil pyaudio
     
     
     
     
+#phase 2: enregistrer un fichier audio grace a un micro
+
+
