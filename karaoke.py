3c769621b86c143b7832f5d102a9728de0d96376
Unknown <andy.chal@orange.fr>
2018-04-02 17:12:26 +0200

    Commentaire sur le main

--- karaoke.py
+++ karaoke.py
@@ -13,33 +13,51 @@ class MainWindows(QDialog, mainWindows.Ui_Dialog):
         self.setupUi(self)
         self.progressBar.setValue(0)
 
+    #Rechercher un nouveau fichier audio
     def fileSearchClick(self):
         file_dialog = QFileDialog(self)
+
+        #Définition des différents filtres
         file_dialog.setNameFilters(["Fichier audio (*.wav)"])
         file_dialog.selectNameFilter("Fichier audio (*.wav)")
+
+        #ouverture de la boite de dialogue
         if (file_dialog.exec_()):
+            #Définition de la variable global contenant le fichier à lire
             global AudioFile
             AudioFile = file_dialog.selectedFiles().first()
 
+    # Ecouter la musique à lire
     def ecouterClick(self):
+        # si aucun fichier n'à été définit
         if (AudioFile == None):
+            # on ouvre la boite de dialogue demandant le fichier
             self.fileSearchClick()
 
+        # on écoute ensuite le fichier
         if (AudioFile != None):
             print("ecouter")
 
+    # On veut jouer sur la musique actuelle
     def jouerClick(self):
+        # si aucun fichier n'à été définit
         if (AudioFile == None):
+            # on ouvre la boite de dialogue demandant le fichier
             self.fileSearchClick()
 
+        # on joue sur le fichier
         if (AudioFile != None):
             print("jouer")
 
+# si ce fichier correpond au fichier d'exécution python
 if __name__ == "__main__":
+    # on définit notre fenetre
     app=QApplication(sys.argv)
     form=MainWindows()
     form.show()
 
+    # on définit le fichier audio
     AudioFile = None;
 
+    # on exécute l'application
     app.exec_()
