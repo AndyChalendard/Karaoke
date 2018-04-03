8042730d22f674f825227f69e60be7b9ac86b5ac
DKTPasquall <pascal.ruffie3@gmail.com>
2018-04-03 17:02:24 +0200

    Jouer l enregistrement
    permet de faire lire l enregistrement

---
+++ Audio/play record.py
@@ -0,0 +1,23 @@
+#ce fichier est une fonction a mettre apres le premier fichier audio
+#si on veut replay ce qu'on a enregistrer via le micro
+#dans ce cas on doit mettre en return de la fonction de l autre fichier le tableau frame.
+
+def lecturerecord(frames) :
+    
+    play=pyaudio.PyAudio()
+    CHUNK = 1024
+    RATE = 44100
+    FORMAT = pyaudio.paInt16            
+    CHANNELS = 2
+    
+    stream_play=play.open(format=FORMAT,
+                      channels=CHANNELS,
+                      rate=RATE,
+                      output=True)
+                      
+    for data in frames: 
+        stream_play.write(data)
+        
+    stream_play.stop_stream()
+    stream_play.close()
+    play.terminate()
