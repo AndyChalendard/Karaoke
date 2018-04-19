58eaacb2cb48e2999ecce2e3ef5283af3bbfb9c3
DKTPasquall <pascal.ruffie3@gmail.com>
2018-04-19 23:56:40 +0200

    tests config v2

--- configuration.py
+++ configuration.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 #travaux en cours
+#je teste ces codes demain matin, car pb ac mon pc , je switcheri avec mon autre tkt
 
 #ce qui est derriere les # sont des test ou des suggestions
 def creerdictionnaire():
@@ -9,23 +10,26 @@ def creerdictionnaire():
     dico={'chunk' : 1024 , 'rate' : 8000 , 'channels' : 1}
     #print (dico)
     #print dico['chunk']
+    return dico
 
 class config():
 
     def __init__(self):
-        creerdictionnaire()
+        dico=creerdictionnaire()
         print("test")
 
     def close(self):
         print("close")
 
-    def setValue(self, name, value):
+    def setValue(self, name, value):    #attribution d une valeur à une clé
         dico=creerdictionnaire()
         dico[name]=value
         print("set")
 
     def getValue(self, name):
+        valeur=dico.get(name)
         print("return value")
+        return valeur
 
 
 # si ce fichier correpond au fichier d'exécution python
