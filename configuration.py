c890f52801cbbf75f1b3512965d04895f26f8e4b
DKTPasquall <pascal.ruffie3@gmail.com>
2018-04-20 10:50:25 +0200

    config v4.1
    elimination d erreurs

--- configuration.py
+++ configuration.py
@@ -1,45 +1,45 @@
 # -*- coding: utf-8 -*-
 
 #travaux en cours
-#je teste ces codes demain matin, car pb ac mon pc , je switcherai avec mon autre tkt
 
 #ce qui est derriere les # sont des test ou des suggestions
 def creerDictionnaire():
     #dictionnaire={}         #creer un dictionnaire vide
     #valeurs par defaut : 
     dico={'chunk' : 1024 , 'rate' : 8000 , 'channels' : 1}
-    #print (dico)
+    print (dico)
     #print dico['chunk']
     return dico
     
 def affichageDico(dictionnaire):
-    for obj in dictionnaire.items()
-        print "'{0}' : '{1}'".format(key,value)
+    for key,value in dictionnaire.items():
+        print ("'{0}' : '{1}'".format(key,value))
     
 def dataOnFile(dictionnaire):           #ecrit dans un fichier les elements du dictionnaire
     fichier=open("data.txt", "a")
-    for obj in dictionnaire.items():
+    for key,value in dictionnaire.items():
         fichier.write("'{0}':'{1}'".format(key,value))      # on ecrit clé:valeur pour chaque couple
     fichier.close()
 
 
 class config():
+    
+    dico=creerDictionnaire()
 
     def __init__(self):
-        dico=creerDictionnaire()
         print("test")
 
     def close(self):
-        dataOnFile(dico)
+        dataOnFile(self.dico)
         self.close()
         print("close")
 
     def setValue(self, name, value):    #attribution d une valeur à une clé
-        dico[name]=value
+        self.dico[name]=value
         print("set")
 
     def getValue(self, name):               #recuperation d une valeur d une clé dans le dictionnaire
-        valeur=dico.get(name)
+        valeur=self.dico.get(name)
         print("return value")
         return valeur
 
