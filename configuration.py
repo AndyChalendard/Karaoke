1a6b700cc81d8f2981e8b5f7e4e4a0c6c754e84e
DKTPasquall <pascal.ruffie3@gmail.com>
2018-04-20 11:19:09 +0200

    config v4.2
    pb avec close et ecriture ds un fichier

--- configuration.py
+++ configuration.py
@@ -2,20 +2,20 @@
 
 #travaux en cours
 
-#ce qui est derriere les # sont des test ou des suggestions
+#ce qui est derriere les # sont des tests
 def creerDictionnaire():
-    #dictionnaire={}         #creer un dictionnaire vide
+    #dictionnaire={}        
     #valeurs par defaut : 
     dico={'chunk' : 1024 , 'rate' : 8000 , 'channels' : 1}
     print (dico)
-    #print dico['chunk']
+    #print (dico['chunk'])             # affichage d un element
     return dico
     
 def affichageDico(dictionnaire):
     for key,value in dictionnaire.items():
         print ("'{0}' : '{1}'".format(key,value))
     
-def dataOnFile(dictionnaire):           #ecrit dans un fichier les elements du dictionnaire
+def dataOnFile(dictionnaire):           #ecrit dans un fichier les elements du dictionnaire,pas l'air de marcher
     fichier=open("data.txt", "a")
     for key,value in dictionnaire.items():
         fichier.write("'{0}':'{1}'".format(key,value))      # on ecrit clé:valeur pour chaque couple
@@ -30,17 +30,19 @@ class config():
         print("test")
 
     def close(self):
-        dataOnFile(self.dico)
+        #dataOnFile(self.dico)
         self.close()
         print("close")
 
     def setValue(self, name, value):    #attribution d une valeur à une clé
         self.dico[name]=value
         print("set")
+        #print(self.dico[name])
 
     def getValue(self, name):               #recuperation d une valeur d une clé dans le dictionnaire
         valeur=self.dico.get(name)
         print("return value")
+        #print(valeur)
         return valeur
 
 
@@ -50,4 +52,11 @@ class config():
 # si ce fichier correpond au fichier d'exécution python
 if __name__ == "__main__":
     print("Test config")
-
+    
+    class5=config()
+    print (class5.dico)
+    class5.getValue('chunk')
+    class5.setValue('chunk',1023)
+    print(class5.dico)
+    dataOnFile(class5.dico)
+    class5.close()
