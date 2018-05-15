29e3e7e00cb1bbb5dfb7e9d8a02afe418f2f5cfb
DKTPasquall <pascal.ruffie3@gmail.com>
2018-05-15 23:21:21 +0200

    modifs reglees

--- configuration.py
+++ configuration.py
@@ -4,9 +4,7 @@
     #dico={'chunk' : 1024 , 'rate' : 8000 , 'channels' : 1}
     #print (dico['chunk'])             # affichage d un element
 
-#On suppose qu'on a en entree un fichier avec des valeurs par defaut dedans (j ai supposé 3 constantes pour l instant) (ligne32)
-#je suppose egalemnt que le type d un objet est representé sur 3 caracteres ( int , str....)  (ligne 38)
-#j ai supposé enfin , qu on avait que deux types : str, int (lignes 44)
+
 
 class config():
 
@@ -27,27 +25,31 @@ class config():
     def __init__(self):
         print("initialisation, lecture des valeurs du fichier")
         fichier0=open("param.conf","r")
-       # print("testboucle")
+        print("testboucle")
         ligne=fichier0.readline()
-        for i in range(3):                        #tq on est pas en fin de fichier , a la limite compter le nb de lignes
-           # print("debuttour")
-            p0=ligne.find(' : ')        #p0 est l indice du premier ' : ' , qui est un séparateur d'information
-            p1=p0+3+ligne[p0+3:].find(' : ') #p1 est le deuxieme
-            cleligne=ligne[:p0]         #nickel
-            valeurligne=ligne[p0+3:p1]
-            typeligne=ligne[p1+3:p1+6]
+        while(ligne!=""):                        #tq on est pas en fin de fichier 
+            print("debuttour")
+            p0=ligne.find('ß')        #p0 est l indice du premier 'ß' , qui est un séparateur d'information
+            p1=p0+1+ligne[p0+1:].find('ß') #p1 est le deuxieme
+            p2=p1+1+ligne[p1+1:].find('\n')
+            cleligne=ligne[:p0]        
+            valeurligne=ligne[p0+1:p1]
+            typeligne=ligne[p1+1:p2]
             #print(p0)
             #print(p1)
+            #print(p2)
             #print(cleligne)
             #print(valeurligne)
             #print(typeligne)
             if (typeligne=="int"):
                 self.dico[cleligne]=int(valeurligne)
-            else:                               #autre type de donnees a prevoir
-                self.dico[cleligne]=valeurligne #a voir 
+            elif (typeligne=="float"):                             
+                self.dico[cleligne]=float(valeurligne)
+            else:
+                self.dico[cleligne]=valeurligne
             ligne=fichier0.readline()
-           # print("fintour")
-       # print("fintestboucle")
+            print("fintour")
+        print("fintestboucle")
         
 
 
@@ -58,7 +60,7 @@ class config():
             value_type=str(valtype)
             value_type=value_type.replace('<class \'','')
             value_type=value_type.replace('\'>','')
-            fichier.write("{0} : {1} : ".format(key,value))      # on écrit clé:valeur:type pour chaque couple
+            fichier.write("{0}ß{1}ß".format(key,value))      # on écrit clé:valeur:type pour chaque couple
             fichier.write(value_type)
             fichier.write("\n")
         fichier.close()
@@ -92,4 +94,3 @@ if __name__ == "__main__":
     class5.affichageDico()
     class5.close()
 
-#voir comment est representé la fin d un fichier , la fin d une ligne dans un fichier , voir tous les types d objets possibles
